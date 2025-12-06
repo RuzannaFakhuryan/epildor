f338bf6b38bd30e8b79f4313da9a41bbec84fe27
RuzannaFakhuryan <ruzanfaxuryan@gmail.com>
2025-12-06 13:55:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Liste des images dans le dossier image/
-const images = ["https://postimg.cc/d7skyLBf", "photo2.jpg", "photo3.jpg"];
-let indexImage = 0;
 
-// Sélectionner l'image et le bouton
-const maImage = document.getElementById("maImage");
-const bouton = document.getElementById("changerImage");
-
-// Changer l'image au clic
-bouton.addEventListener("click", function() {
-    indexImage++;
-    if (indexImage >= images.length) {
-        indexImage = 0; // revenir à la première image
-    }
-    maImage.src = "image/" + images[indexImage];
-});
