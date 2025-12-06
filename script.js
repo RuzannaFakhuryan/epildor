9235b1d7e0750a0db1fa1c2949bccf04c83a9a05
RuzannaFakhuryan <ruzanfaxuryan@gmail.com>
2025-12-06 13:10:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Formulaire de contact
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Merci pour votre message ! Nous vous répondrons rapidement.');
+  this.reset();
+});
