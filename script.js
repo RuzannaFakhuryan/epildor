0d40dbd013bdfeb7f57a9edad8aebd457d666d2a
RuzannaFakhuryan <ruzanfaxuryan@gmail.com>
2025-12-06 13:52:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// Formulaire de contact
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Merci pour votre message ! Nous vous répondrons rapidement.');
-  this.reset();
+// Liste des images dans le dossier image/
+const images = ["https://postimg.cc/d7skyLBf", "photo2.jpg", "photo3.jpg"];
+let indexImage = 0;
+
+// Sélectionner l'image et le bouton
+const maImage = document.getElementById("maImage");
+const bouton = document.getElementById("changerImage");
+
+// Changer l'image au clic
+bouton.addEventListener("click", function() {
+    indexImage++;
+    if (indexImage >= images.length) {
+        indexImage = 0; // revenir à la première image
+    }
+    maImage.src = "image/" + images[indexImage];
 });
